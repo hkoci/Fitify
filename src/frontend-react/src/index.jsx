57121806a97a1794704374e371e48efb19f21004
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-01-26 12:36:41 +0000

    Using only Node.js version 14 for CI

--- src/frontend-react/src/index.jsx
+++ src/frontend-react/src/index.jsx
@@ -1,7 +1,7 @@
 //Import React libraries including ReactRouter
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 //Stylesheets
 import './index.css';

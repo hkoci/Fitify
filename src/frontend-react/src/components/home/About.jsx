96ec0166ae5f61f622e517677a4eca348216ece3
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-01-26 12:27:03 +0000

    Instantiate React Components, added React Router
    Folder structure for React added. React components and router working.

---
+++ src/frontend-react/src/components/home/About.jsx
@@ -0,0 +1,12 @@
+//Import React library
+import React from 'react';
+
+//Create About class using Component instace
+class About extends React.Component {
+   render() {
+      return <h1> About Component </h1>
+   }
+}
+
+//Export class
+export default About;
---
+++ src/frontend-react/src/index.jsx
@@ -0,0 +1,26 @@
+//Import React libraries including ReactRouter
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+
+//Stylesheets
+import './index.css';
+
+//Components (react-router)
+import Landing from './components/home/Landing';
+import About from './components/home/About';
+import Help from './components/home/Help';
+
+//Setup React Router routes
+const routs = (
+   <Router>
+      <div>
+         <Route exact path="/" component={Landing} />
+         <Route path="/about" component={About} />
+         <Route path="/help" component={Help} />
+      </div>
+   </Router>
+);
+
+//Render the Routes in the document root
+ReactDOM.render(routs, document.getElementById('root'));

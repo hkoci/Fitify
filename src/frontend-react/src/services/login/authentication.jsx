2416f8d97a4de4146674c110a7331ee90d0b63ef
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-01-27 13:53:42 +0000

    Include Error catches for incorrect users

--- src/frontend-react/src/services/login/authentication.jsx
+++ src/frontend-react/src/services/login/authentication.jsx
@@ -20,6 +20,32 @@ class AuthenticationService {
                 'Content-Type': 'application/json;charset=UTF-8',
                 "Access-Control-Allow-Origin": "*",
             }
+        }).then((response) => {
+            //The post method was successful and the AccessToken is recieved
+            console.log(response.data[0])
+
+            //TODO: Something with the AccessToken to be stored and used permanently for later communication to endpoints
+        })
+        .catch((error) => {
+            //The post method was not successful and some error has occured
+
+            /*
+            This could be Unauthorised (401) - incorrect user/password
+            This might be MethodArgumentNotValidException (400) - wrong arguments sent
+            Or something else (e.g. Spring server is down.)
+            */
+
+            //To get error status, we can log it to the console using console.log(error.response.status)
+            //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+
+            if(error.response.status==401){
+                //Incorrect username/password
+                //Redirect to login component and warn user that combination is incorrect
+            }else(error.response.status==400){
+                //Axios returned incorrect arguments
+                //Check our code in the data arguments to make sure username and password is named correctly.
+                //Mainly react server error
+            }
         })
     }   
 

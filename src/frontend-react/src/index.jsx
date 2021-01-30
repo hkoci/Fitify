2640438c2c46a53bf38887bd16072c3b9f0a995a
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-01-30 21:33:56 +0000

    Changed authenticated React routes implementation

--- src/frontend-react/src/index.jsx
+++ src/frontend-react/src/index.jsx
@@ -1,7 +1,7 @@
 //Import React libraries including ReactRouter
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 //Stylesheets
 import './index.css';
@@ -13,10 +13,12 @@ import Help from './components/home/Help';
 
 //Fitify Components
 import Login from './components/fitify-app/Login';
+import Register from './components/fitify-app/Register';
+import Stepper from './components/fitify-app/stepper';
 import Dashboard from './components/fitify-app/Dashboard';
 
-//Import Authentication required router
-import AuthenticationRouter from './services/login/authentication-router';
+//Import Authentication class to determine if user is authenticated
+import Authentication from './services/login/authentication';
 
 //Setup React Router routes
 const routs = (
@@ -26,10 +28,24 @@ const routs = (
          <Route path="/about" component={About} />
          <Route path="/help" component={Help} />
          <Route path="/app/login" component={Login} />
-         <AuthenticationRouter path="/app/dashboard" component={Dashboard} />
+         <Route path="/app/register" component={Register} />
+         <Route path="/app/stepper" component={Stepper} />
+         <AuthenticatedRoute path="/app/dashboard" component={Dashboard} />
       </div>
    </Router>
 );
+
+//Authenticated Routing functionality
+function AuthenticatedRoute({ component: Component, ...rest }) {
+   return (
+     <Route {...rest} render={props => Authentication.getAuthenticationStatus() ? (<Component {...props} />) 
+         : (
+           <Redirect to={{ pathname: "/app/login", state: { from: props.location } }} />
+         )
+       }
+     />
+   );
+ }
 
 //Render the Routes in the document root
 ReactDOM.render(routs, document.getElementById('root'));

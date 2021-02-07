42469e057cc4765ebcd1862e9c96f08a2cea98ab
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-07 20:45:16 +0000

    Removed old backend

--- src/backend-spring/healthapp/src/main/java/uk/ac/brunel/group7/healthapp/repos/UserFriendRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.brunel.group7.healthapp.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.ac.brunel.group7.healthapp.domain.UserFriend;
-
-
-public interface UserFriendRepository extends JpaRepository<UserFriend, Long> {
-    // add custom queries here
-}

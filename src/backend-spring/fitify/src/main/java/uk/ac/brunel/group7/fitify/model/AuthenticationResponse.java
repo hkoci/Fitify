6d53153e37b612b3ddecb48638b135768aa038d9
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-07 20:27:58 +0000

    Added JWT Response Model

---
+++ src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/model/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package uk.ac.brunel.group7.fitify.model;
+
+
+public class AuthenticationResponse {
+
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(final String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}

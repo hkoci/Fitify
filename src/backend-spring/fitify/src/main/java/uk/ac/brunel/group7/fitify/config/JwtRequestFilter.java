8aed7ac02d338bfea362ac2ae6f4c1bceb1e3ecd
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-07 20:27:32 +0000

    Added JWT Spring Configuration

---
+++ src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/config/JwtRequestFilter.java
@@ -0,0 +1,62 @@
+package uk.ac.brunel.group7.fitify.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import uk.ac.brunel.group7.fitify.service.JwtUserDetailsService;
+import uk.ac.brunel.group7.fitify.service.JwtTokenService;
+
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final JwtTokenService jwtTokenService;
+
+    public JwtRequestFilter(final JwtUserDetailsService jwtUserDetailsService,
+                            final JwtTokenService jwtTokenService) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain chain) throws ServletException, IOException {
+        // look for Bearer auth header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        final String token = header.substring(7);
+        final String username = jwtTokenService.validateTokenAndGetUsername(token);
+        if (username == null) {
+            // validation failed or token expired
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // set user details on spring security context
+        final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+        final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // continue with authenticated user
+        chain.doFilter(request, response);
+    }
+
+}

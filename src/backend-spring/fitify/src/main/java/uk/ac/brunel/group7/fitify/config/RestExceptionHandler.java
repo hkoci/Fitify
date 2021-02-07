155d8b4fee20ceb2b0c2d2319ac8de1de111a72e
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-07 20:24:53 +0000

    Added Java configuration properties

---
+++ src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/config/RestExceptionHandler.java
@@ -0,0 +1,120 @@
+package uk.ac.brunel.group7.fitify.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestControllerAdvice(annotations = RestController.class)
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(final ResponseStatusException exception) {
+        final ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setHttpStatus(exception.getStatus().value());
+        errorResponse.setException(exception.getClass().getSimpleName());
+        errorResponse.setMessage(exception.getMessage());
+        return new ResponseEntity<>(errorResponse, exception.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException exception) {
+        final BindingResult bindingResult = exception.getBindingResult();
+        final List<FieldErrorResponse> fieldErrorResponses = bindingResult.getFieldErrors()
+                .stream()
+                .map(error -> {
+                    final FieldErrorResponse fieldError = new FieldErrorResponse();
+                    fieldError.setErrorCode(error.getCode());
+                    fieldError.setField(error.getField());
+                    return fieldError;
+                })
+                .collect(Collectors.toList());
+        final ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setHttpStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setException(exception.getClass().getSimpleName());
+        errorResponse.setFieldErrors(fieldErrorResponses);
+        return errorResponse;
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable exception) {
+        exception.printStackTrace();
+        final ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.setException(exception.getClass().getSimpleName());
+        return errorResponse;
+    }
+
+    public static class FieldErrorResponse {
+
+        private String field;
+        private String errorCode;
+
+        public String getField() {
+            return field;
+        }
+
+        public void setField(String field) {
+            this.field = field;
+        }
+
+        public String getErrorCode() {
+            return errorCode;
+        }
+
+        public void setErrorCode(String errorCode) {
+            this.errorCode = errorCode;
+        }
+
+    }
+
+    public static class ErrorResponse {
+
+        private Integer httpStatus;
+        private String exception;
+        private String message;
+        private List<FieldErrorResponse> fieldErrors;
+
+        public Integer getHttpStatus() {
+            return httpStatus;
+        }
+
+        public void setHttpStatus(final Integer httpStatus) {
+            this.httpStatus = httpStatus;
+        }
+
+        public String getException() {
+            return exception;
+        }
+
+        public void setException(final String exception) {
+            this.exception = exception;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(final String message) {
+            this.message = message;
+        }
+
+        public List<FieldErrorResponse> getFieldErrors() {
+            return fieldErrors;
+        }
+
+        public void setFieldErrors(List<FieldErrorResponse> fieldErrors) {
+            this.fieldErrors = fieldErrors;
+        }
+
+    }
+
+}

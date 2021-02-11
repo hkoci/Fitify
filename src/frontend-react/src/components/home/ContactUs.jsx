b79a304b4a4f477ea3f88ed58a287c0ee32483ef
FlopRe <reichenauerfilip@gmail.com>
2021-02-11 17:11:26 +0000

    Container

--- src/frontend-react/src/components/home/ContactUs.jsx
+++ src/frontend-react/src/components/home/ContactUs.jsx
@@ -1,10 +1,15 @@
 //Import React library
 import React from 'react';
+import Container from '@material-ui/core/Container';
 
 //Create Help class using Component instace
 class ContactUs extends React.Component {
    render() {
-      return <h1> Contact Us </h1>
+      return (
+        <Container maxWidth="lg">
+          <h1> Contact Us </h1>
+        </Container>
+      )
    }
 }
 

a31c3cd66c4142ddef04f51272f5256aabd60fa7
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-22 17:01:36 +0000

    Displaying user variables to sessionStorage

--- src/frontend-react/src/services/login/authentication.jsx
+++ src/frontend-react/src/services/login/authentication.jsx
@@ -30,6 +30,9 @@ class Authentication {
             //Store username in localStorage
             sessionStorage.setItem("CurrentUsername", username)
 
+            //Store User Details in session
+            this.getUserDetails()
+
             //Change Axios configuration to use accesstoken as Authorisation Bearer
             this.axiosRequestTokenHeader("Bearer " + response.data.accessToken)
         })
@@ -87,6 +90,63 @@ class Authentication {
         sessionStorage.removeItem("CurrentUsername");
     }
 
+    //Method to get Authorisation Bearer token from JWT
+    //Request it using username and password parameters
+    getUserDetails(){
+        //console.log('URL' +  `${SpringHostURL}/api/users/username/` + sessionStorage.getItem("CurrentUsername"))
+        //Post the username and password as json to the auth endpoint
+        return axios.get(`${SpringHostURL}/api/users/username/` + sessionStorage.getItem("CurrentUsername"),{
+        },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //"Access-Control-Allow-Origin": "*"
+            }
+        }).then((response) => {
+            //The username is retrieved successfully
+            //Store username in localStorage
+            sessionStorage.setItem("UserID", response.data.id)
+            sessionStorage.setItem("FirstName", response.data.firstName)
+            sessionStorage.setItem("LastName", response.data.lastName)
+            sessionStorage.setItem("Gender", response.data.gender)
+            sessionStorage.setItem("Email", response.data.emailAddress)
+            sessionStorage.setItem("DOB", response.data.dob)
+            sessionStorage.setItem("FitPoints", response.data.fitPoints)
+            sessionStorage.setItem("friend", response.data.friend)
+            sessionStorage.setItem("apperance", response.data.apperance)
+            sessionStorage.setItem("notification", response.data.notification)
+            sessionStorage.setItem("health", response.data.health)
+        })
+        .catch((error) => {
+
+            //The post method was not successful and some error has occured
+
+            /*
+            This could be Unauthorised (401) - incorrect user/password
+            This might be MethodArgumentNotValidException (400) - wrong arguments sent
+            Or something else (e.g. Spring server is down.)
+            */
+
+            //To get error status, we can log it to the console using console.log(error.response.status)
+            //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+
+            if(error.response.status === 401){
+                console.log("Error 401 - Incorrect user credentials")
+                throw error
+                //Incorrect username/password
+            }else if(error.response.status === 400){
+                console.error("Error 400 - Incorrect React fronend arguments")
+                throw error
+                //Axios returned incorrect arguments
+                //Check our code in the data arguments to make sure username and password is named correctly.
+                //Mainly react server error
+            }else{
+                console.error("Error: " + error.response.status + " - " + error.response)
+                //Some other error to report back
+                throw error
+            }
+        })
+    }
 }
 
 export default new Authentication()

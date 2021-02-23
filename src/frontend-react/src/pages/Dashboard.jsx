6ac316b6acbfe93c341ec0eebec4a03c94e330c5
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-23 20:29:37 +0000

    Removed unnecessary container import

--- src/frontend-react/src/pages/Dashboard.jsx
+++ src/frontend-react/src/pages/Dashboard.jsx
@@ -5,16 +5,15 @@ import Navbar from '../components/Navbar';
 
 import FirstTimeSetup from '../components/FirstTimeSetup';
 
-import Container from '@material-ui/core/Container';
-
 //Create Landing class using Component instace
 class Dashboard extends React.Component {
+
   render() {
     return (
       <React.Fragment>
         <Navbar title='Dashboard' searchField='true' />
-        <FirstTimeSetup/>    
-      </React.Fragment>    
+        <FirstTimeSetup/>
+      </React.Fragment>
     );
   }
 }

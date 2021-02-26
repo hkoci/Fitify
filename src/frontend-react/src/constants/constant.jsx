6d44ad64f98f4fda46f2d4c4c87b4da4f1d8b867
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-26 11:44:35 +0000

    Included documentation for group members

--- src/frontend-react/src/constants/constant.jsx
+++ src/frontend-react/src/constants/constant.jsx
@@ -1,8 +1,12 @@
-//TODO: Change the constant URL with 
+//TODO: Team members working on a different machine to the one being hosted on - change the constant URL here!!!
 
-//Setup constant for Spring Host - this will be changed in production *Include port but no endpoints!*
+/*
+Setup constant for Spring Host - this will be changed in production *Include port but no endpoints!*
 
-//When testing on the local machine, use the localhost address
+When testing on the local machine, use the localhost address
 
-//When testing on other machines on your network - use your internal LAN IP Address!
+When testing on other machines on your network - replace localhost with your internal LAN IP Address! 
+You will need to either allow your IP address in the Spring Security CORS config (JWTSecurityConfig.java) or use a browser extension to ignore CORS (unsafe!).
+*/
+
 export const SpringHostURL = 'http://localhost:8080'

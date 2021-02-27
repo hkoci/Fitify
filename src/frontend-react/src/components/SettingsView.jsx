05388f383ada0f10983d6285e7e8dfcd775287e6
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-27 10:59:16 +0000

    Included marketing settings
    Added user marketing settings to the settings component

--- src/frontend-react/src/components/SettingsView.jsx
+++ src/frontend-react/src/components/SettingsView.jsx
@@ -4,6 +4,10 @@ import React from 'react';
 //Import Material-ui theming 
 import { makeStyles } from '@material-ui/core/styles';
 
+//Import Material-ui preloader
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 //Import material-ui typography
 import Typography from '@material-ui/core/Typography';
 
@@ -15,6 +19,8 @@ import Grid from '@material-ui/core/Grid';
 //Import Material-ui Switches
 import Switch from '@material-ui/core/Switch';
 
+//Import Settings
+import MarketingSettings from '../services/settings/MarketingSettings'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,31 +30,82 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(3),
       color: '#404040',
     },
+
+    headingsTextAlt: {
+      paddingTop: theme.spacing(6),
+      padding: theme.spacing(3),
+      color: '#404040',
+    },
+
     settingLabel: {
       padding: theme.spacing(2),
       textAlign: 'left',
       color: theme.palette.text.secondary,
+    },
+
+    switch: {
+      marginBottom: theme.spacing(2.5),
     }
   }));
 
-export default function Navbar(props) {
+  
 
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-  });
+export default function Navbar(props) {
 
   const classes = useStyles();
 
+  //const [state, setState] = React.useState(MarketingSettings.getMarketingSettings());
+ 
+  const [state, setState] = React.useState({
+    achievementsPreference: MarketingSettings.getMarketingState('achievementsPreference'),
+    dailyEmailProgressPreference: MarketingSettings.getMarketingState('dailyEmailProgressPreference'),
+    marketingEmailPreference: MarketingSettings.getMarketingState('marketingEmailPreference'),
+    marketingRoadmapPreference: MarketingSettings.getMarketingState('marketingRoadmapPreference'),
+    progressPreference: MarketingSettings.getMarketingState('progressPreference'),
+    weeklyEmailProgressPreference: MarketingSettings.getMarketingState('weeklyEmailProgressPreference'),
+  }); 
+
+  // Convert 'checked' to 'value' before performing handleChange
+  const handleCheckedChange = (event) => {
+    //Perform changes to the Spring Backend (through Axios Put modifier)
+    MarketingSettings.setMarketingState(event.target.name,event.target.checked);
+    //Change internal React state
+    handleChange(event);
+  };
+
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
-  };
+  }; 
 
   return (
     <div className={classes.root}>
     <Container fixed>
+
           <Typography variant="h5" component="h5" className={classes.headingsText}>
-            Settings
+            Personal
+          </Typography>
+          <Paper>
+            <Grid container spacing={3}>
+              <Grid item xs={10} xl={11}>
+                <Typography className={classes.settingLabel}>
+                  Some very long setting text goes here. Test a couple of words in a setence - thanks for using Fitify :)
+                </Typography>
+              </Grid>
+              <Grid item xs={2} xl={1}>
+                <Switch
+                  checked={state.marketingEmailPreference}
+                  onChange={handleCheckedChange}
+                  name="marketingEmailPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  textalignContent='right'
+                  alignItems='right'
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+
+          <Typography variant="h5" component="h5" className={classes.headingsTextAlt}>
+            Appearance
           </Typography>
           <Paper>
             <Grid container spacing={3}>
@@ -69,6 +126,105 @@ export default function Navbar(props) {
               </Grid>
             </Grid>
           </Paper>
+
+          <Typography variant="h5" component="h5" className={classes.headingsTextAlt}>
+            Notifications
+          </Typography>
+          <Paper>
+            <Grid container spacing={3}>
+              <Grid item xs={10} xl={11}>
+                <Typography className={classes.settingLabel}>
+                  Some very long setting text goes here. Test a couple of words in a setence - thanks for using Fitify :)
+                </Typography>
+              </Grid>
+              <Grid item xs={2} xl={1}>
+                <Switch
+                  checked={state.checkedA}
+                  onChange={handleChange}
+                  name="checkedA"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  textalignContent='right'
+                  alignItems='right'
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+
+          <Typography variant="h5" component="h5" className={classes.headingsTextAlt}>
+            Marketing
+          </Typography>
+          <Paper>
+
+            <Grid container spacing={3}>
+              <Grid item xs={10} xl={11}>
+                <Typography className={classes.settingLabel}>
+                  📩 Marketing communications
+                </Typography>
+                <Typography className={classes.settingLabel}>
+                  📆 Daily health progress
+                </Typography>
+                <Typography className={classes.settingLabel}>
+                  📅 Weekly health progress
+                </Typography>
+                <Typography className={classes.settingLabel}>
+                  🏆 Achievements gained
+                </Typography>
+                <Typography className={classes.settingLabel}>
+                  🚗 Roadmap development plans
+                </Typography>
+                <Typography className={classes.settingLabel}>
+                  🔧 Current development progress
+                </Typography>
+              </Grid>
+              <Grid item xs={2} xl={1}>
+                <Switch
+                  checked={MarketingSettings.getMarketingState('dailyEmailProgressPreference')}
+                  onChange={handleCheckedChange}
+                  name="marketingEmailPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+                <Switch
+                  checked={state.dailyEmailProgressPreference}
+                  onChange={handleCheckedChange}
+                  name="dailyEmailProgressPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+                <Switch
+                  checked={state.weeklyEmailProgressPreference}
+                  onChange={handleCheckedChange}
+                  name="weeklyEmailProgressPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+                <Switch
+                  checked={state.achievementsPreference}
+                  onChange={handleCheckedChange}
+                  name="achievementsPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+                <Switch
+                  checked={state.marketingRoadmapPreference}
+                  onChange={handleCheckedChange}
+                  name="marketingRoadmapPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+                <Switch
+                  checked={state.weeklyEmailProgressPreference}
+                  onChange={handleCheckedChange}
+                  name="weeklyEmailProgressPreference"
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  className={classes.switch}
+                />
+              </Grid>
+
+              
+            </Grid>
+          </Paper>
+
     </Container>
     </div>
   );
--- src/frontend-react/src/services/settings/MarketingSettings.jsx
+++ src/frontend-react/src/services/settings/MarketingSettings.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 //Setup constant for Spring Host - this will be changed in production *Include port but no endpoints!*
-const SpringHostURL = 'http://localhost:8080'
+import { SpringHostURL } from '../../constants/constant'
 
 class MarketingSettings {
 
@@ -12,18 +12,75 @@ class MarketingSettings {
 
         await this.initialiseSettingMappings(sessionStorage.getItem('CurrentUsername')).then(response => marketingID = response.marketing);
 
-        return this.getMarketingData(marketingID)
+        const marketingResponse = await this.getMarketingData(marketingID)
+
+        //await console.log(marketingResponse)
+
+        return marketingResponse
     }
 
     //Method to get marketingEmailPreference
-    async getMarketingEmailPreference(){
-        var marketingEmailPreference, marketingID;
+    async getMarketingState(state){
+        var marketingID, value;
 
+        //Get the marketingID record number from the current user
         await this.initialiseSettingMappings(sessionStorage.getItem('CurrentUsername')).then(response => marketingID = response.marketing);
 
-        await this.getMarketingData(marketingID).then(response => marketingEmailPreference = response.marketingEmailPreference);
+        //Store the response from the marketing data of this user temporarily
+        const marketingResponse = await this.getMarketingData(marketingID)
 
-        return marketingEmailPreference
+        //Check which state to change
+        if(state === 'achievementsPreference'){
+            value = marketingResponse.achievementsPreference;
+        }else if(state === 'dailyEmailProgressPreference'){
+            value = marketingResponse.dailyEmailProgressPreference;
+        }else if(state === 'marketingEmailPreference'){
+            value = marketingResponse.marketingEmailPreference;
+        }else if(state === 'marketingRoadmapPreference'){
+            value = marketingResponse.marketingRoadmapPreference;
+        }else if(state === 'progressPreference'){
+            value = marketingResponse.progressPreference;
+        }else if(state === 'weeklyEmailProgressPreference'){
+            value = marketingResponse.weeklyEmailProgressPreference;
+        }else{
+            //do not display any values - state is incorrect
+        }
+
+        console.log(state,value)
+
+        //Return the value of this state
+        return value;
+    }
+
+    //Method to set marketing data
+    async setMarketingState(state,value){
+        var marketingID;
+
+        //Get Marketing mapping id
+        await this.initialiseSettingMappings(sessionStorage.getItem('CurrentUsername')).then(response => marketingID = response.marketing);
+
+        //Store the response from the marketing data of this user temporarily
+        const marketingResponse = await this.getMarketingData(marketingID)
+
+        //Check which state to change
+        if(state === 'achievementsPreference'){
+            marketingResponse.achievementsPreference = value;
+        }else if(state === 'dailyEmailProgressPreference'){
+            marketingResponse.dailyEmailProgressPreference = value;
+        }else if(state === 'marketingEmailPreference'){
+            marketingResponse.marketingEmailPreference = value;
+        }else if(state === 'marketingRoadmapPreference'){
+            marketingResponse.marketingRoadmapPreference = value;
+        }else if(state === 'progressPreference'){
+            marketingResponse.progressPreference = value;
+        }else if(state === 'weeklyEmailProgressPreference'){
+            marketingResponse.weeklyEmailProgressPreference = value;
+        }else{
+            //do not change any values - state is incorrect
+        }
+
+        //Update the marketing record accordingly
+        await this.updateMarketingRecord(marketingID,marketingResponse);
     }
     
     //Method to get user setting mappings
@@ -47,6 +104,22 @@ class MarketingSettings {
     async getMarketingData(id){
         return axios.get(`${SpringHostURL}/api/users/settings/marketing/${id}`,{
         },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //"Access-Control-Allow-Origin": "*"
+            }
+        }).then(response => response.data)
+        //The post method was not successful and some error has occured
+
+        //To get error status, we can log it to the console using console.log(error.response.status)
+        //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+    .catch(error => console.error(error))
+    }
+
+    //Method to modify marketing response data
+    async updateMarketingRecord(id,marketingResponse){
+        return axios.put(`${SpringHostURL}/api/users/settings/marketing/${id}`,marketingResponse,{
             headers: {
                 //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
                 'Content-Type': 'application/json;charset=UTF-8',

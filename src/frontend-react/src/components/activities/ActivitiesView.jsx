e65fcafb61e358591ff3d2116640be3538cf31b0
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-27 10:57:05 +0000

    Added add button on activities
    Included floating add button on activities component

--- src/frontend-react/src/components/activities/ActivitiesView.jsx
+++ src/frontend-react/src/components/activities/ActivitiesView.jsx
@@ -4,8 +4,22 @@ import React from 'react';
 //Import Material-ui theming 
 import { makeStyles } from '@material-ui/core/styles';
 
+//Import Material-ui preloader
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+//Import material-ui typography
+import Typography from '@material-ui/core/Typography';
+
 //Import Material-ui layout
 import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+
+//Import Material-ui Floating Button
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
@@ -14,6 +28,11 @@ const useStyles = makeStyles((theme) => ({
       '& > *': {
         margin: theme.spacing(1),
       },
+    },
+    extendedIcon: {
+      position: "fixed",
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
     },
   }));
 
@@ -25,7 +44,9 @@ export default function Activities(props) {
     <div className={classes.root}>
       <CssBaseline />
       <Container fixed>
-
+        <Fab color="primary" aria-label="add" className={classes.extendedIcon}>
+        <AddIcon />
+        </Fab>
       </Container>
     </div>
   );

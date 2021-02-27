0fb9f5a221c486918497a7d56994d96bcb60d620
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-27 12:20:28 +0000

    Added base MuiTheme styling
    The theme styling is to be made - the base code has been added to Settings for now.

--- src/frontend-react/src/pages/Settings.jsx
+++ src/frontend-react/src/pages/Settings.jsx
@@ -6,14 +6,33 @@ import SettingsView from '../components/SettingsView';
 
 import Box from '@material-ui/core/Box';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+//Testing Theme code (TODO)
+const primaryColour = sessionStorage.getItem("PrimaryColour");
+const secondaryColour = sessionStorage.getItem("SecondaryColour");
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#E33E7F'
+    },
+    secondary: {
+      main: '#E33E7F'
+    }
+  }
+});
+
 //Create Landing class using Component instace
 class Settings extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar title='Settings' searchField='true' />
-        <Box m={2} />
-        <SettingsView />
+        <MuiThemeProvider theme={theme}>
+          <Navbar title='Settings' searchField='true' />
+          <Box m={2} />
+          <SettingsView />
+        </MuiThemeProvider>
       </React.Fragment>
     );
   }

dbd34cb789af703f3a1af7411e87cf6831f92929
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-02-28 19:27:25 +0000

    Created ThemeService

--- src/frontend-react/src/services/settings/ThemeSettings.jsx
+++ src/frontend-react/src/services/settings/ThemeSettings.jsx
@@ -54,6 +54,41 @@ class ThemeSettings {
         return value;
     }
 
+    //Method overloading - username parameter method
+    async getAppearanceState2(state,username){
+        var appearanceID, value;
+
+        await this.initialiseSettingMappings(username).then(response => appearanceID = response.apperance);
+
+        await console.log(appearanceID)
+
+        //Store the response from the marketing data of this user temporarily
+        const appearanceResponse = await this.getAppearanceData(appearanceID)
+
+        await console.log(appearanceResponse)
+
+        //Check which state to change
+        if(state === 'primaryHexColour'){
+            value = appearanceResponse.primaryHexColour;
+        }else if(state === 'secondaryHexColour'){
+            value = appearanceResponse.secondaryHexColour;
+        }else if(state === 'darkMode'){
+            value = appearanceResponse.darkMode;
+        }else if(state === 'highContrast'){
+            value = appearanceResponse.highContrast;
+        }else if(state === 'textSize'){
+            value = appearanceResponse.textSize;
+        }else if(state === 'avatarDefaultColour'){
+            value = appearanceResponse.avatarDefaultColour;
+        }else{
+            //do not display any values - state is incorrect
+            return null
+        }
+
+        //Return the value of this state
+        return value;
+    }
+
     //Method to get user appearance data
     async getAppearanceData(id){
         return axios.get(`${SpringHostURL}/api/users/settings/appearance/${id}`,{
@@ -138,19 +173,21 @@ class ThemeSettings {
     }
 
     async storeAppearance(){
-        const primaryColour = await this.getAppearanceState('primaryHexColour')
-        const secondaryColour = await this.getAppearanceState('secondaryHexColour')
-        const dark = await this.getAppearanceState('darkMode')
-        const contrast = await this.getAppearanceState('highContrast')
-        const textSize = await this.getAppearanceState('textSize')
+        var appearanceID;
 
-        sessionStorage.setItem("primaryHexColour", primaryColour)
-        sessionStorage.setItem("secondaryHexColour", secondaryColour)
-        sessionStorage.setItem("darkMode", dark)
-        sessionStorage.setItem("highContrast", contrast)
-        sessionStorage.setItem("textSize", textSize)
-    }
+        await this.initialiseSettingMappings(sessionStorage.getItem('CurrentUsername')).then(response => appearanceID = response.apperance);
 
+        const appearanceResponse = await this.getAppearanceData(appearanceID)
+      
+        console.log(appearanceID)
+        console.log(appearanceResponse)
+      
+        sessionStorage.setItem("primaryHexColour", appearanceResponse.primaryHexColour)
+        sessionStorage.setItem("secondaryHexColour", appearanceResponse.secondaryHexColour)
+        sessionStorage.setItem("darkMode", appearanceResponse.darkMode)
+        sessionStorage.setItem("highContrast", appearanceResponse.highContrast)
+        sessionStorage.setItem("textSize", appearanceResponse.textSize)
+      }
 }
 
 export default new ThemeSettings()

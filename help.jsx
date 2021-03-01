1beaa2a4a3d062b23e3d31f8cec13dae6267908d
Abznya <58242465+Abznya@users.noreply.github.com>
2021-03-01 04:39:33 +0000

    Help page react component
    I changed the html to the react component

---
+++ help.jsx
@@ -0,0 +1,47 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./help.css";
+
+class Help extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="12f02607-438d-4bc4-91e3-34c2e71a7727" className="help">        <div data-layer="c0f86720-745b-410b-b1cf-76dba0ed8e87" className="background"></div>
+        <div data-layer="d967fbb4-afcf-4d98-91b9-0d1f91d70838" className="logoPng"></div>
+        <div data-layer="56db0a31-de2a-4959-9098-01be16b0a291" className="beYourBest">Be your best!</div>
+        <div data-layer="8c825ac7-2f8f-4681-8f2f-664f7ebd6b2c" className="ifYouAreExperiencingTechnicalDifficultiesWithOurServicesPleaseDontHesitateToContactUsWeWillBeMoreThanWillingToHelpYouOutAndToProvideTheBestCareToResolveYourProblem">If you are experiencing technical difficulties with our services, please don't hesitate to contact us. We will be more than willing to help you out and to provide the best care to resolve your problem.</div>
+        <div data-layer="70d4739f-51e6-404d-8df6-386de20b70d5" className="needAssistance">Need Assistance?</div>
+        <div data-layer="aeea2b90-5ed2-45cd-9c7d-986c1f879e02" className="feelFreeToContactUsAndWeWillRespondToYouAsSoonAsWeCan">Feel free to contact us and we will respond to you as soon as we can. </div>
+        <div data-layer="52e56e73-f079-4810-a4cb-3051271f044f" className="contactUs">Contact us</div>
+        <div data-layer="98c9080d-e135-4e12-9b26-11a91d1510b7" className="home">Home</div>
+        <div data-layer="2b19e254-98fc-410d-9c0e-faabd82be01a" className="signIn">Sign in</div>
+        <div data-layer="4583eae8-9d18-41bb-b9e7-2cd3b0084f69" className="review">Review</div>
+        <div data-layer="12790a36-338a-456f-85bb-c927ed8255c7" className="helpb842ee8b">Help</div>
+        <svg data-layer="b64251b2-c507-4e46-8d4c-d8316d25d824" preserveAspectRatio="none" viewBox="-2 -2 66 4" className="line1"><path d="M 0 0 L 62 0"  /></svg>
+</div>
+
+    );
+  }
+}
+
+Help.propTypes = {
+
+}
+
+Help.defaultProps = {
+
+}
+
+
+export default Help;
+          

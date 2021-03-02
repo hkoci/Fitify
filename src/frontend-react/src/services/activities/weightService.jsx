86910cc81f82048e60cce85711675841f9c88915
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-03-02 11:21:40 +0000

    Implemented Activity/Weight Creation CRUD
    CRUD to get data needs to be implemented

--- src/frontend-react/src/services/activities/weightService.jsx
+++ src/frontend-react/src/services/activities/weightService.jsx
@@ -28,18 +28,18 @@ class weightService {
         //Create the activity record
         const activityResponse = await this.createActivity(startDateTime, endDateTime, activityType, moodRating, caloriesBurnt, sessionStorage.getItem('UserID'))
 
-        await console.log(activityResponse)
+        await console.log('Activity: ', activityResponse)
 
         //Create the parent weight activity record
         const weightResponse = await this.createWeight(activityResponse, weight, description)
 
-        await console.log(weightResponse)
+        await console.log('Weight: ', weightResponse)
 
         return weightResponse
     }
     
 
-    //Method to create marketing data
+    //Method to create Activity data
     async createActivity(
         startDateTime,
         endDateTime,
@@ -48,16 +48,53 @@ class weightService {
         caloriesBurnt,
         userID
     ){
+        return axios.post(`${SpringHostURL}/api/activities`,{
+            //Json Body content
+            "userID": userID,
+            "activityStart": startDateTime,
+            "activityEnd": endDateTime,
+            "activityType": activityType,
+            "moodRating": moodRating,
+            "caloriesBurnt": caloriesBurnt
+        },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //Set Bearer Token JWT auth
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        }).then(response => response.data)
+            //The post method was not successful and some error has occured
 
+            //To get error status, we can log it to the console using console.log(error.response.status)
+            //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+        .catch(error => console.error(error))
     }
 
-    //Method to create weight activity
+    //Method to create Weight activity
     async createWeight(
         activityID,
         weight,
         description
     ){
+        return axios.post(`${SpringHostURL}/api/activities/weight`,{
+            //Json Body content
+            "activityID": activityID,
+            "weight": weight,
+            "description": description
+        },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //Set Bearer Token JWT auth
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        }).then(response => response.data)
+            //The post method was not successful and some error has occured
 
+            //To get error status, we can log it to the console using console.log(error.response.status)
+            //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+        .catch(error => console.error(error))
     }
 
 }

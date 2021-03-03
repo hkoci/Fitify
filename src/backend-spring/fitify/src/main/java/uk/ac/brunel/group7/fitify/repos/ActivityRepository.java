50cf19b4d196b73071f22ca71ce00e7fa96f147a
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-03-03 11:11:03 +0000

    Remove unnecessary import (user in activity)

--- src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/repos/ActivityRepository.java
+++ src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/repos/ActivityRepository.java
@@ -3,8 +3,6 @@ package uk.ac.brunel.group7.fitify.repos;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.ac.brunel.group7.fitify.domain.Activity;
-import uk.ac.brunel.group7.fitify.domain.User;
-
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     Optional<Activity> findByUserID(Long userID);

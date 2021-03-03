25d92ca2cc99ac369df2f05a5358804044dd385a
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-03-03 11:18:18 +0000

    Create WeightGet service for reading weight user data
    Implemented just filtering the UserID activity of the current user - the actual weight filter needs to be implemented.

---
+++ src/frontend-react/src/services/activities/weightGet.jsx
@@ -0,0 +1,74 @@
+//Import Axios library to proxy requests to Spring server
+import axios from 'axios'
+
+//Setup constant for Spring Host - this will be changed in production *Include port but no endpoints!*
+import { SpringHostURL } from '../../constants/constant'
+
+class WeightGet {
+
+    //Method to get all activities by the current user
+    async getUserActivity(){
+        //Get Activity Data
+        const activityDataRes = await this.getActivityData()
+
+        //Filter just results by this user
+        const userActivityRes = await activityDataRes.filter(function(item){
+            return item.userID == sessionStorage.getItem('UserID');         
+        });
+
+        //Debug info
+        await console.log(activityDataRes)
+        await console.log(userActivityRes)
+
+        return userActivityRes
+    }
+
+    //Method to get all weight measurements by the current user
+    async getUserWeight(){
+
+    }
+
+    //TODO: Method to merge filtered data and return this
+    async getUserData(){
+
+    }
+
+    //Method to get activity data
+    async getActivityData(){
+        return axios.get(`${SpringHostURL}/api/activities`,{
+        },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //Set Bearer Token JWT auth
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        }).then(response => response.data)
+        //The post method was not successful and some error has occured
+
+        //To get error status, we can log it to the console using console.log(error.response.status)
+        //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+    .catch(error => console.error(error))
+    }
+
+    //Method to get activity weight data
+    async getWeightData(){
+        return axios.get(`${SpringHostURL}/api/activities/weight`,{
+        },{
+            headers: {
+                //Set the post content as application/json (Spring will not recognise text for this auth endpoint PostMapping)
+                'Content-Type': 'application/json;charset=UTF-8',
+                //Set Bearer Token JWT auth
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        }).then(response => response.data)
+        //The post method was not successful and some error has occured
+
+        //To get error status, we can log it to the console using console.log(error.response.status)
+        //To get the error msg, we can log it to the console using console.log(error.response.data.error)
+    .catch(error => console.error(error))
+    }
+
+}
+
+export default new WeightGet()

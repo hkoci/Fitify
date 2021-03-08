1ca42d54d963dec2bad7a96a65fc7fce1da90692
Filip Reichenauer <reichenauerfilip@gmail.com>
2021-03-08 21:02:14 +0000

    Change ml to L to reflect backend

--- src/frontend-react/src/components/water/WaterIntakeTable.jsx
+++ src/frontend-react/src/components/water/WaterIntakeTable.jsx
@@ -15,7 +15,7 @@ export default function RowsGrid() {
     const tableColumns = [
         { field: "activityStart", headerName: "Date/Time", description: 'The date/time of the weight measurement.', type: 'dateTime', flex: 0.9},
         { field: "moodRating", headerName: "Mood Rating", description: 'Mood rating is out of 5, with 5 being the most motivated.', type: 'number', flex: 0.5},
-        { field: "waterIntake", headerName: "Amount (ml)", description: 'Amount is measured in milliliters.', type: 'number', flex: 0.5},
+        { field: "waterIntake", headerName: "Amount (L)", description: 'Amount is measured in litres.', type: 'number', flex: 0.5},
         { field: "description", headerName: "Description", description: 'Optional description entered by the user.', sortable: false, flex: 1},
         ];
 

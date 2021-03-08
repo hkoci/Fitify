34d85d3e65677c98ab1da6673364092abf93e69c
Filip Reichenauer <reichenauerfilip@gmail.com>
2021-03-08 19:52:18 +0000

    Add WaterRepository class (repos)

---
+++ src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/repos/WaterRepository.java
@@ -0,0 +1,8 @@
+package uk.ac.brunel.group7.fitify.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.ac.brunel.group7.fitify.domain.Water;
+
+
+public interface WaterRepository extends JpaRepository<Water, Long> {
+}

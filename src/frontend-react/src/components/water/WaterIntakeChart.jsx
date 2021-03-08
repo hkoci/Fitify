74e3e079ff64f9065a5e36d84a1a4ae39582bf91
Filip Reichenauer <reichenauerfilip@gmail.com>
2021-03-08 19:28:53 +0000

    Add Water intake chart

---
+++ src/frontend-react/src/components/water/WaterIntakeChart.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+//Import Recharts for line graph rendering
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+
+//Import WaterIntakeGet service
+import WaterIntakeGet from '../../services/activities/waterIntakeGet';
+
+//Grey Tooltip text (fixes white Theme css)
+import './tooltip.css';
+
+export default function RowsGrid() {
+
+		//State for chart data to be appended on
+		const [chartData,setChartData] = useState([]);
+
+		//Load chart data before render
+		useEffect(() => {
+			WaterIntakeGet.getUserData().then(dataResponse => {
+				//Update Chart data
+				setChartData(dataResponse);
+			})
+			}, []);
+
+		return (
+			<ResponsiveContainer width="99%" height={400}>
+                <AreaChart width={730} height={250} data={chartData}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }} >
+                    <defs>
+                        <linearGradient id="Water Intake" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#4290f5" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#4290f5" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="activityStart"/>
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="7 7" />
+                    <Tooltip />
+                    <Legend />
+                    <Area type="monotone" dataKey="Water Intake" name="Water Intake (in ml)" stroke="#8884d8" fillOpacity={1} fill="url(#WaterIntake)" unit=" ml"/>
+                </AreaChart>
+			</ResponsiveContainer>
+		);
+	
+}

7d39ad879e2ea3e40b01ac060dbf51353023c166
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-03-09 15:25:29 +0000

    Revert commit

--- src/backend-spring/fitify/src/main/java/uk/ac/brunel/group7/fitify/repos/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.brunel.group7.fitify.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.ac.brunel.group7.fitify.domain.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
